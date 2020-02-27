7ff1ccaa4a3cff479843b652264e4d71d5db8f72
SleepyJen <jennifer.ni0924@gmail.com>
2020-02-26 20:48:47 -0800

    added path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
+const path = require('path');
 
 const mongoose = require('mongoose');
 const connection = mongoose.connection;
